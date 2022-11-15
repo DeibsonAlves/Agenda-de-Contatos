c54bdceadc2935e1f2e950672c2269019a09af2f
Deibson Pereira Alves <deibsonalves36@gmail.com>
2022-11-15 12:50:32 -0300

    Ainda trabalhando nisto...

--- Agenda da Contatos/index.php
+++ Agenda da Contatos/index.php
@@ -8,42 +8,32 @@
     <link rel="stylesheet" href="estilo.css/">
 </head>
 <body>
-    <section id="direita">
+    <section id="esquerda">
         <form>
             <h2>CADASTRAR PESSOA</h2>
-            <label> </label>
-            <input>
-            <label> </label>
-            <input>
-            <label> </label>
-            <input>
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome">
+            <label for="telefone">Telefone</label>
+            <input type="text" name="telefone" id="telefone">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email">
+            <input type="submit" value="cadastrar">
         </form>
     
     </section>
 
-        <section id="esquerda">
+        <section id="direita">
+            <table> 
+                <tr id="titulo">
+                    <td>NOME</td>
+                    <td>TELEFONE</td>
+                    <td colspan="2">EMAIL</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><a href="">Editar</a><a href="">Excluir</a></td>
+                </tr>
+            </table>
         </section>
-<?php
-try 
-{
-    $pdo = new PDO("mysql:dbname=CRUDPDO;host=localhost","root","");
-
-}
-
-catch(PDOException $e) {
-echo "Erro com banco de dados: ".$e->getMessage();
-}
-
-catch(Exception $e){
-    echo "Erro genérico: ".$e->getMessage();  
-}
-
-$pdo->prepare()
-
-
-
-
-?>
-
-</body>
-</html>
