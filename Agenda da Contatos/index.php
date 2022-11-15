fc8839ae269005ea440e71ede473bdfb2d3a539f
Deibson Pereira Alves <deibsonalves36@gmail.com>
2022-11-14 23:29:08 -0300

    Ainda criando...

--- Agenda da Contatos/index.php
+++ Agenda da Contatos/index.php
@@ -4,10 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastro Pessoas</title>
+    <link rel="stylesheet" href="estilo.css/">
 </head>
 <body>
+    <section id="direita">
+        <form>
+            <h2>CADASTRAR PESSOA</h2>
+            <label> </label>
+            <input>
+            <label> </label>
+            <input>
+            <label> </label>
+            <input>
+        </form>
     
+    </section>
+
+        <section id="esquerda">
+        </section>
 <?php
 try 
 {
@@ -22,6 +37,11 @@ echo "Erro com banco de dados: ".$e->getMessage();
 catch(Exception $e){
     echo "Erro genérico: ".$e->getMessage();  
 }
+
+$pdo->prepare()
+
+
+
 
 ?>
 
